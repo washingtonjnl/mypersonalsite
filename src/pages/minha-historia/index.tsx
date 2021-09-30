4afebfcba3b0ea69e8873a142cdbdcc65fab93cb
Washington Junior <washingtonjrt@gmail.com>
2021-09-30 02:53:03 -0300

    update: my history copy

--- src/pages/minha-historia/index.tsx
+++ src/pages/minha-historia/index.tsx
@@ -25,9 +25,9 @@ function MyHistory() {
             Oi, meu nome é <span>Washington Junior...</span>
           </h1>
           <p>
-            Tenho 19 anos e atualmente moro no Rio de Janeiro. Sou amante de
-            tecnologia, inovação e sempre gosto de estar por dentro do que
-            acontece nessa área. Adoro resolver problemas de forma prática e
+            Tenho 20 anos e atualmente moro no Rio de Janeiro. Sou amante de
+            tecnologia, inovação e gosto de estar sempre por dentro do que
+            acontece nessas áreas. Adoro resolver problemas de forma prática e
             criativa, por isso, me encontrei quando o design cruzou minha vida.
             Para mim, um bom design existe para conectar pessoas e transmitir
             ideias de forma simples, funcional e – principalmente – acessível
@@ -35,11 +35,13 @@ function MyHistory() {
           </p>
           <p>
             Já passei pelas mais diversas áreas dentro do design, desde
-            impressos, produtos, mídias digitais e até criação de marcas (quando
-            tornei mais sólidos meus fundamentos teóricos e melhorei a
-            integração do lado estratégico de negócios ao meu trabalho) e nos
-            ultimos anos, atuo com design de produtos{' '}
-            <strong>digitais orientados ao usuário.</strong>
+            impressos, produtos, mídias digitais e até criação de marcas
+            (quando, pela primeira vez, tornei mais sólidos meus fundamentos
+            teóricos e melhorei a integração do lado estratégico de negócios ao
+            meu trabalho). Nos ultimos anos, atuo com{' '}
+            <strong>
+              design de produtos digitais orientados à experiência do usuário.
+            </strong>
           </p>
         </ContentTitle>
         <ResponsiveImage
@@ -48,26 +50,29 @@ function MyHistory() {
           alt="Design para web e interfaces aplicadas"
         />
         <Paragraph>
-          Ainda quando criança, sempre gostei de trabalhar e o empreendedorismo
-          me ensinou a fazer negócios, ser mais criativo e me envolver bastante
-          com tecnologia e ferramentas que otimizam e aumentam minha
-          produtividade.
+          Ainda quando criança, comecei a gostar de trabalhar para ajudar outras
+          pessoas e, com o tempo, o empreendedorismo me ensinou a fazer
+          negócios, ser mais criativo e me envolver bastante com tecnologia e
+          ferramentas que otimizam e aumentam minha efetividade.
         </Paragraph>
         <Paragraph>
           Nos últimos tempos, quando comecei a atuar na elaboração de produtos
           digitais, desenvolvi cada vez mais minhas{' '}
           <strong>
-            metodologias de pesquisa, análise e planejamento de negócio
+            metodologias de pesquisa, análise de métricas e mapeamento de
+            oportunidades nas jornadas de produto,
           </strong>{' '}
-          articulando sempre com outras equipes seguindo as metodologias ágeis.
+          articulando sempre com outras equipes atuando com metodologias ágeis.
         </Paragraph>
         <Paragraph>
           Ao trabalhar implementando os elementos fundamentais do design em
-          interfaces que prezem pela boa usabilidade e consigam proporcionar
-          boas experiências, melhorei minha capacidade de abstração e{' '}
+          interfaces que colaboram para uma melhor usabilidade e, a partir
+          disso, facilitar boas experiências, melhorei minha capacidade de
+          abstração e{' '}
           <strong>
-            empatia para observar as necessidades do usuário com mapeamentos,
-            testes e pesquisas quantitativas e qualitativos.
+            empatia para observar as necessidades do usuário com testes e
+            entrevistas quantitativas e qualitativos, além de outras
+            metodologias de pesquisa comportamentais e atitudinais.
           </strong>
         </Paragraph>
         <ResponsiveImage
@@ -77,19 +82,22 @@ function MyHistory() {
         />
         <Paragraph>
           Por acreditar que uma comunicação clara e integrada é essencial para o
-          sucesso de uma equipe, sempre busco entender as demandas de cada time
-          responsavel dentro dos projetos para criar documentos e feedbacks que
-          sejam realmente impactantes para a evolução do ecossistema de produto.
+          sucesso de uma equipe, sempre busco entender as demandas de cada par
+          responsavel dentro dos projetos para ajudar a estruturar processos,
+          documentações e feedbacks que sejam realmente impactantes e relevantes
+          na medida certa para a evolução do ecossistema de produto.
         </Paragraph>
         <Paragraph>
           Foi por conta da minha curiosidade e proatividade que me envolvi de
           maneira muito próxima com times de desenvolvimento e isso me ajudou
           bastante a{' '}
-          <strong>entender a viabilidade técnica das soluções.</strong>{' '}
+          <strong>
+            entender a viabilidade técnica das soluções que construíamos.
+          </strong>{' '}
           Versionamento, abstração, divisão de responsabilidades e documentação
           para descentralização do conhecimento foram só algumas das coisas que
-          me engrandeceram quando me aproximei e comecei a caminhar pela trilha
-          do desenvolvimento de software.
+          me engrandeceram quando me aproximei e comecei a me interessar pelas
+          trilhas do desenvolvimento de software.
         </Paragraph>
         <Paragraph>
           Como sempre gostei de aproveitar da tecnologia para otimizar minha
@@ -99,9 +107,10 @@ function MyHistory() {
             para dentro do design
           </strong>{' '}
           melhorou muito a minha produtividade. Seja criando pequenos sistemas
-          para tratar dados, otimizar processos repetitivos ou até na criação de{' '}
+          para tratar dados de pesquisas, otimizar processos repetitivos ou até
+          na criação de{' '}
           <strong>
-            <i>styleguides </i>
+            <i>design systems </i>
             versáteis e robustos.
           </strong>
         </Paragraph>
